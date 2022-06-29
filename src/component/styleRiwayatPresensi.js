e277a8d2b92a7b2150e986a9ee8eb1c3597f15b3
Frinaldo Sinaga <84637791+Sinagafrinaldo@users.noreply.github.com>
2022-06-29 13:38:57 +0700

    style riwayat presensi

---
+++ src/component/styleRiwayatPresensi.js
@@ -0,0 +1,83 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#F5FBFF',
+        height: '100%'
+    },
+
+    container2: {
+        backgroundColor: 'white',
+        height: '100%',
+    },
+
+    ikonAdd: {
+        position: 'absolute',
+        bottom: 30,
+        right: 40
+    },
+    input: {
+        borderWidth: 1,
+        margin: 20,
+        padding: 20
+    },
+    box_tanggal: {
+        margin: 10,
+        padding: 10,
+        borderWidth: 1,
+        borderRadius: 7,
+        borderColor: 'grey',
+    },
+    button: {
+        backgroundColor: '#118eeb',
+        borderRadius: 5,
+        alignItems: 'center',
+        margin: 10,
+        padding: 10
+    },
+
+    list1: {
+        backgroundColor: 'white',
+        flexDirection: 'row',
+        padding: 15,
+        margin: 10,
+        borderRadius: 8,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 3,
+        },
+        shadowOpacity: 0.27,
+        shadowRadius: 4.65,
+
+        elevation: 6,
+    },
+
+    list2: {
+        backgroundColor: '#118eeb',
+        height: 26,
+        width: 5,
+    },
+
+    data: {
+        paddingLeft: 8,
+        width: '98%',
+    },
+
+    btnHapus: {
+        backgroundColor: 'brown',
+        padding: 10,
+        marginTop: 10,
+        borderRadius: 8,
+    },
+
+    cari: {
+        flexDirection: 'row',
+        display: 'flex',
+        alignSelf: 'center',
+
+    }
+})
+
+export default styles
