477d56df517672e9a983e78f95da70a4e9433362
119140037 - Joy Ravelo Tarigan <56099222+joyrav09@users.noreply.github.com>
2022-07-02 17:40:36 +0700

    Add files via upload

--- src/pages/keluar.js
+++ src/pages/keluar.js
@@ -34,23 +34,24 @@ const Keluar = () => {
         }, [])
     );
 
-    const handleSignUp = () => {
-        createUserWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
-                const user = userCredential.user;
-                // console.log('Registered with:', user.email);
-                alert("Selamat akun anda telah terdaftar!")
-            })
-            .catch(error => alert(error.message))
-    }
-    const signOutUser = async () => {
-        try {
-            await auth.signOut();
+    // const handleSignUp = () => {
+    //     createUserWithEmailAndPassword(auth, email, password)
+    //         .then((userCredential) => {
+    //             const user = userCredential.user;
+    //             // console.log('Registered with:', user.email);
+    //             alert("Selamat akun anda telah terdaftar!")
+    //         })
+    //         .catch(error => alert(error.message))
+    // }
+    // const signOutUser = async () => {
+    //     try {
+    //         await auth.signOut();
 
-        } catch (e) {
-            console.log(e);
-        }
-    }
+    //     } catch (e) {
+    //         console.log(e);
+    //     }
+    // }
+
     const handleSignIn = () => {
         signInWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
@@ -111,11 +112,12 @@ const Keluar = () => {
                     onRefresh={onRefresh}
                 />
             }
-            style={{ backgroundColor: 'white', height: 1000, }}>
+            contentContainerStyle={{backgroundColor:'white', flexGrow: 1}}>
+            
             {status == false && (
                 <View style={styles.container}>
-                    <View style={styles.wrap1}>
-                        <Text style={styles.login}>Selamat Datang</Text>
+                    <View style={styles.header}>
+                        <Text style={styles.title}>Selamat Datang</Text>
 
                         <Image
                             style={styles.stretch}
@@ -133,14 +135,14 @@ const Keluar = () => {
                         />
                     </TouchableOpacity>
                     
-                    <View style={styles.card}>
-                        <Text style={{ marginBottom: 5, alignSelf: 'center', color: '#118eeb', fontWeight: 'bold', fontSize: 18 }}>LOGIN</Text>
-                        <View style={{ width: '70%', height: 1.5, alignSelf: 'center', backgroundColor: '#D7DBDD' }}></View>
-                        <Text style={{ color: 'gray', marginTop: 20, marginBottom: 5 }}>Email</Text>
-
+                    <View style={styles.card_login}>
+                        <Text style={styles.title_login }>LOGIN</Text>
+                        <View style={styles.line}></View>
+                        
+                        <Text style={styles.title_email}>Email</Text>
                         <View style={{ flexDirection: 'row' }}>
                             <Ionicons
-                                style={styles.ikonMenu}
+                                style={styles.icon_box}
                                 name="mail"
                                 size={20}
                                 color="#D7DBDD"
@@ -148,16 +150,15 @@ const Keluar = () => {
                             <TextInput
                                 onChangeText={email => onChangeNip(email)}
                                 value={email}
-                                style={styles.boxnip}
+                                style={styles.box_input}
                                 placeholder='Email...'
                             />
                         </View>
 
-                        <Text style={{ color: 'gray', marginTop: 20, marginBottom: 5 }}>Kata Sandi</Text>
-
+                        <Text style={styles.title_password}>Kata Sandi</Text>
                         <View style={{ flexDirection: 'row' }}>
                             <Ionicons
-                                style={styles.ikonMenu}
+                                style={styles.icon_box}
                                 name="eye-off"
                                 size={20}
                                 color="#D7DBDD"
@@ -165,22 +166,23 @@ const Keluar = () => {
                             <TextInput
                                 onChangeText={password => onChangePassword(password)}
                                 value={password}
-                                style={styles.boxnip}
+                                style={styles.box_input}
                                 placeholder='Kata Sandi...'
                                 secureTextEntry
                             />
                         </View>
-                        <TouchableOpacity style={styles.tombol} onPress={handleSignIn}>
-                            <Text style={styles.tekslogin}>Login</Text>
+
+                        <TouchableOpacity style={styles.touch} onPress={handleSignIn}>
+                            <Text style={styles.text_login}>Login</Text>
                         </TouchableOpacity>
-                        <View style={{ display: 'flex', flexDirection: 'row', marginTop: 20 }}>
+                        <View style={styles.registration}>
                             <Text>Belum punya akun?</Text>
                             <View>
                                 <TouchableOpacity
                                     onPress={() => {
                                         navigation.navigate("Daftar");
                                     }}>
-                                    <Text style={styles.teksdaftar}>  Daftar</Text>
+                                    <Text style={styles.text_registration}>  Daftar</Text>
                                 </TouchableOpacity>
                             </View>
                         </View>
@@ -192,26 +194,26 @@ const Keluar = () => {
 
             {status == true && (
                 <View>
-                    <View style={{ justifyContent: 'center', padding: 40, }}>
-                        <View style={styles2.cardlog}>
-                            <Text style={{ textAlign: 'center', color: '#f75555', fontSize: 18 }}>Logout</Text>
-                            <View style={{ borderBottomWidth: 1, borderColor: '#e0e0e0', marginVertical: 10 }}></View>
-                            <Text style={styles2.textout}>Apakah anda yakin ingin keluar?</Text>
-                            <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around' }}>
-                                <TouchableOpacity style={styles2.tombol2} onPress={() => {
+                    <View style={styles2.container}>
+                        <View style={styles2.card_logout}>
+                            <Text style={styles2.title_logout}>Logout</Text>
+                            <View style={styles2.line}></View>
+                            <Text style={styles2.text_out}>Apakah anda yakin ingin keluar?</Text>
+                            <View style={styles2.style_touch}>
+                                <TouchableOpacity style={styles2.touch_cancel} onPress={() => {
 
 
                                     navigation.navigate('Beranda')
                                 }}>
-                                    <Text style={styles2.teksbatal}>Batal</Text>
+                                    <Text style={styles2.text_cancel}>Batal</Text>
                                 </TouchableOpacity>
-                                <TouchableOpacity style={styles2.tombol} onPress={() => {
+                                <TouchableOpacity style={styles2.touch_logout} onPress={() => {
 
 
                                     signOutUser()
                                     setStatus(false)
                                 }}>
-                                    <Text style={styles2.tekslogin}>Logout</Text>
+                                    <Text style={styles2.text_logout}>Logout</Text>
                                 </TouchableOpacity>
                             </View>
                         </View>
