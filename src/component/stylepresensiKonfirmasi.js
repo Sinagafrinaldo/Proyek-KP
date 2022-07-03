d3be6b29a9bc6476bbe55a58780015b6bcba67a5
119140037 - Joy Ravelo Tarigan <56099222+joyrav09@users.noreply.github.com>
2022-07-03 15:49:39 +0700

    Add files via upload

--- src/component/stylepresensiKonfirmasi.js
+++ src/component/stylepresensiKonfirmasi.js
@@ -2,7 +2,9 @@ import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
 const styles = StyleSheet.create({
-    card: {
+    container: {
+        flex:1,
+        display:'flex',
         height: '100%',
         padding:20,
         backgroundColor: 'white',
@@ -12,33 +14,33 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
 
-    ikon1: {
-        marginTop: -21,
-    },
-    cekin: {
-        backgroundColor: '#FFD600',
-        paddingVertical: 10,
-        paddingHorizontal: 30,
-        borderRadius: 10,
-        marginVertical: 20
-    },
-
-    stretch: {
-        resizeMode: 'stretch',
-        width: 230,
-        height:260,
-    },
-
-    stretch2: {
+    image_confirm_absent: {
         resizeMode: 'stretch',
         width: 350,
         height:260,
     },
-    cekin1: {
+
+    text_confirm_absent :{
+        fontSize: 18, 
+        fontFamily: 'poppinssemibold', 
+        color: 'gray', 
+        textAlign: 'center',
+    },
+   
+    text_now :{
+        fontSize: 18, 
+        fontFamily: 'poppinssemibold', 
+        color: 'gray', 
+        marginBottom: 40, 
+        textAlign: 'center',
+    },
+     
+    touch_absent: {
         backgroundColor: '#F9A826',
         borderRadius: 10,
+        width:'90%',
         alignItems: 'center',
-        width: '100%',
+        alignSelf:'center',
         shadowColor: "#000",
         shadowOffset: {
             width: 0,
@@ -46,68 +48,74 @@ const styles = StyleSheet.create({
         },
         shadowOpacity: 0.20,
         shadowRadius: 1.41,
-
-elevation: 2,
+        elevation: 2,
     },
-    teksin: {
+
+    text_absent: {
+        color: 'white',
+        fontWeight:'bold',
+        fontFamily: 'poppinssemibold',
+        fontSize: 16,
+        textAlign: 'center',
+        padding:16,
+    },
+
+    image_absent: {
+        resizeMode: 'stretch',
+        width: 230,
+        height:260,
+    },
+
+    text_absent_info:{
+        fontSize: 34, 
+        fontFamily: 'poppinssemibold', 
+        color: '#118eeb', 
+        textAlign: 'center',
+    },
+
+    text_online_info :{
+        fontSize: 18, 
+        fontFamily: 'poppins', 
+        color: 'gray', 
+        marginBottom: 30, 
+        textAlign: 'center',
+    },
+
+    text_welcome :{
+        fontSize: 15, 
+        fontFamily: 'poppinssemibold', 
+        color: 'gray',
+        marginBottom: 8, 
+        textAlign: 'center',
+    },
+
+    text_absent_done: {
         color: '#2D7CF3',
         fontFamily: 'poppinssemibold',
         fontSize: 24,
         textAlign: 'center',
     },
 
-    teksin2: {
-        color: 'white',
-        fontWeight:'bold',
-        fontFamily: 'poppinssemibold',
-        fontSize: 18,
-        textAlign: 'center',
-        padding:12,
+
+    container_verif_false :{
+        padding:10, 
+        flex:1, 
+        display:'flex', 
+        justifyContent: 'center', 
+        alignItems: 'center',
     },
 
-    cekout: {
-        backgroundColor: '#E5E5E5',
-        paddingVertical: 10,
-        paddingHorizontal: 30,
-        borderRadius: 10,
-        marginBottom: 20
+    image_not_verif :{
+        resizeMode: 'stretch',
+        width:260,
+        height: 245
     },
-    teksout: {
-        color: '#AAAAAA',
-        fontWeight: 'bold'
+
+    text_not_verif :{
+        textAlign: 'center', 
+        color :'gray',
+        fontFamily: 'poppins'
     },
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#fff',
-    },
-    button: {
-        margin: 10,
-        paddingHorizontal: 10,
-        paddingVertical: 7,
-        borderRadius: 5,
-        backgroundColor: "#AEDEF4",
-    },
-    text: {
-        color: '#fff',
-        fontSize: 15
-    },
-    title2: {
-        color: 'gray',
-        fontSize: 24,
-        textAlign: 'center',
-        fontWeight: '700'
-    },
-    title3: {
-        color: 'gray',
-        fontSize: 14,
-        textAlign: 'center',
-        marginBottom: 35
-    }
-    // stretch :{
-    //     resizeMode: 'stretch',
-    // },
 })
 
 export default styles
