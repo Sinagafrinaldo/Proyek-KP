958926f7373cd18285fa4cef4ab4c0042a506313
119140037 - Joy Ravelo Tarigan <56099222+joyrav09@users.noreply.github.com>
2022-07-04 22:56:31 +0700

    Add files via upload

--- src/component/styleDaftar.js
+++ src/component/styleDaftar.js
@@ -8,7 +8,7 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
     },
 
-    wrap1: {
+    header: {
         backgroundColor: '#118eeb',
         borderBottomEndRadius: 20,
         borderBottomStartRadius: 20,
@@ -17,7 +17,7 @@ const styles = StyleSheet.create({
 
     },
 
-    login: {
+    title: {
         textAlign: 'center',
         paddingBottom: 20,
         color: 'white',
@@ -41,7 +41,7 @@ const styles = StyleSheet.create({
         top: 50
     },
 
-    card: {
+    card_registration: {
         marginTop: -175,
         marginBottom: 10,
         paddingTop: 25,
@@ -61,7 +61,35 @@ const styles = StyleSheet.create({
 
         elevation: 5,
     },
-    boxnip: {
+
+    title_registration :{
+        marginBottom: 5, 
+        alignSelf: 'center', 
+        color: '#118eeb', 
+        fontWeight: 'bold', 
+        fontSize: 18
+    },
+
+    line : { 
+        width: '70%', 
+        height: 1.5, 
+        alignSelf: 'center', 
+        backgroundColor: '#D7DBDD' 
+    },
+
+    title_box :{
+        color: 'gray', 
+        marginTop: 20, 
+        marginBottom: 5
+    },
+
+    icon_box: {
+        position: 'absolute',
+        bottom: 14,
+        paddingLeft: 10
+    },
+
+    box_input: {
         borderWidth: 1,
         borderRadius: 10,
         width: '100%',
@@ -70,29 +98,30 @@ const styles = StyleSheet.create({
         paddingLeft: 38,
 
     },
-    ikonMenu: {
-        position: 'absolute',
-        bottom: 14,
-        paddingLeft: 10
+
+    select_box : {
+        borderWidth: 1,
+        borderRadius: 10,
+        height:51,
+        width: '100%',
+        borderColor: '#D7DBDD',
+        paddingLeft: 38,
     },
-    tombol: {
+   
+    touch : {
         backgroundColor: '#118eeb',
-        marginTop: 20,
+        marginTop: 35,
         padding: 10,
         alignItems: 'center',
         borderRadius: 10
     },
-    tekslogin: {
+
+    teks_registration: {
         padding: 6,
         color: 'white',
         fontSize: 16,
-        fontWeight: 'bold'
-        ,
-    },
-    teksdaftar: {
-        color: '#1F76C6',
         fontWeight: 'bold',
-    }
+    },
 })
 
 export default styles
